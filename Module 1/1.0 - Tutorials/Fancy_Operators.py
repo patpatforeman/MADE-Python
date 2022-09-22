b95ee34ac8f85ff9c59c82affe74dcb7737834f7
patpatforeman <patrickforeman123@gmail.com>
2022-09-21 21:34:46 -0400

    Initial Commit

---
+++ Module 1/1.0 - Tutorials/Fancy_Operators.py
@@ -0,0 +1,15 @@
+# This is a "python module" that essentially brings in
+# fancier math operations
+import math
+
+# Here is how to do powers the fancy way:
+# The first input is the value you want, and the second is the
+# power you want to raise it to. i.e. 5^2
+power = math.pow(5, 2)
+print("5 squared is: " + str(power))
+
+# Same goes for the square root!
+# This only needs the number
+root = math.sqrt(25)
+print("The square root of 25 is: " + str(root))
+
