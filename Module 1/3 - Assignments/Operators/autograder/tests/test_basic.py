9314570033acd3124f4ec19b12df684195733f48
Jason Forsyth <forsy2jb@jmu.edu>
2022-10-24 16:39:06 -0400

    Add working Autograder

---
+++ Module 1/3 - Assignments/Operators/autograder/tests/test_basic.py
@@ -0,0 +1,45 @@
+import unittest
+from gradescope_utils.autograder_utils.decorators import weight, number
+import student_submission as pp
+
+
+class TestBasic(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @weight(2)
+    @number("1.1")
+    def testfrst_num(self):
+        self.assertIsNotNone(pp.first_number, "Did you change first_number's value?")
+        self.assertAlmostEqual(pp.first_number, 64)
+
+    @weight(2)
+    @number("1.2")
+    def testscnd_num(self):
+        self.assertIsNotNone(pp.second_number, "Did you change second_number's value?")
+        self.assertAlmostEqual(pp.second_number, 6.4)
+
+    @weight(2)
+    @number("1.3")
+    def testthrd_num(self):
+        self.assertIsNotNone(pp.third_number, "Did you change third_number's value?")
+        self.assertAlmostEqual(pp.third_number, 68.4)
+
+    @weight(2)
+    @number("1.4")
+    def testfrth_num(self):
+        self.assertIsNotNone(pp.fourth_number, "Did you change fourth_number's value?")
+        self.assertAlmostEqual(pp.fourth_number, 66.4)
+
+    @weight(2)
+    @number("1.5")
+    def testsqwr_num1(self):
+        self.assertIsNotNone(pp.squared_number_one, "Did you change squared_number_one's value?")
+        self.assertAlmostEqual(pp.squared_number_one, 64)
+
+    @weight(2)
+    @number("1.6")
+    def testsqwr_num2(self):
+        self.assertIsNotNone(pp.squared_number_two, "Did you change squared_number_two's value?")
+        self.assertAlmostEqual(pp.squared_number_two, 64.0)
